31aa122772da2be8f4bdfce3c9977f031378d081
alaayiss <alaamess00@gmail.com>
2020-10-02 22:11:57 +0100

    calcul lib

--- bin/dart.dart
+++ bin/dart.dart
@@ -1,4 +1,11 @@
 
-main() {
+import 'package:dart/calcul.dart';
+import 'package:dart/dart.dart';
 
+
+main() {
+List<int> number = [1,2,3,4,5,6];
+
+  calculator Calculator = calculator();
+  int resault = Calculator.calculate(number, 'add');
 }
---
+++ lib/calcul.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'dart:math';
+
+class calculator{
+ List<String> 
+
+  int calculate(List<int> numbers , String method){
+    int resault = 0;
+switch(method){
+  case "add":
+break;
+  case "sub":
+    break;
+  case "multi":
+    break;
+  case "div":
+    break;
+  default :
+    break;
+
+}
+return resault;
+  }
+
+}
